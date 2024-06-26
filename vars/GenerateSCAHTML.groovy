4dd65c81efd7b1cac0cae54d558387ce5b5edfe7
Kyal Lanum <83832885+kyallanum-MND@users.noreply.github.com>
2024-06-26 17:27:22 -0600

    Update GenerateSCAHTML.groovy

--- vars/GenerateSCAHTML.groovy
+++ vars/GenerateSCAHTML.groovy
@@ -3,11 +3,6 @@ def call() {
   RunPythonScript()
 }
 
-def InstallVirtualEnv() {
-  echo 'Installing Virtual Environment for Python'
-  sh 'python3 -m venv venv && source ./venv/bin/activate'
-}
-
 def RunPythonScript() {
   sh 'mend_generate_html >> index.html'
   publishHTML (target : [allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: '.', reportFiles: 'index.html', reportName: 'SCA Results', reportTitles: 'Mend SCA Scan Report'])
