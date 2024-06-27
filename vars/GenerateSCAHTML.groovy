9b4828567de20cde36c28d04a764b4dc966c2b3a
Kyal Lanum <83832885+kyallanum-MND@users.noreply.github.com>
2024-06-26 18:51:13 -0600

    Update GenerateSCAHTML.groovy

--- vars/GenerateSCAHTML.groovy
+++ vars/GenerateSCAHTML.groovy
@@ -1,4 +1,4 @@
 def call() {
-  sh 'mend_generate_html >> index.html'
+  sh 'mend_generate_html | tee index.html'
   publishHTML (target : [allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: '.', reportFiles: 'index.html', reportName: 'SCA Results', reportTitles: 'Mend SCA Scan Report'])
 }
